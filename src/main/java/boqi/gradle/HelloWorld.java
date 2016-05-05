dbf1001d131ff22b05ccbf156ffdbf05ace83b03
qibo <jonathan.q.bo@gmail.com>
2016-05-05 09:57:21 -0400

    Initialize

--- src/main/java/boqi/gradle/HelloWorld.java
+++ src/main/java/boqi/gradle/HelloWorld.java
@@ -1,9 +1,29 @@
 package boqi.gradle;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
 public class HelloWorld {
+	
+	/**
+	 * Spring boot embeded tomcat will set this application as root application
+	 * which means the url is: http://localhost:8080
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/")
+	@ResponseBody
+	public String sayHello(){
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
-		System.out.println("haha");
+		SpringApplication.run(HelloWorld.class, args);
 	}
 	
 }
